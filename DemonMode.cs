13203bb8dfcc1c32c60a95f2c50e08ef6f93a578
ynot01 <ynot000001@gmail.com>
2025-08-09 02:08:07 -0400

    perf rewrite

--- DemonMode.cs
+++ DemonMode.cs
@@ -3,37 +3,27 @@ using BepInEx.Logging;
 using BepInEx.Unity.IL2CPP;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using HarmonyLib;
 
 namespace demon_mode
 {
 
 	public class DemonMode_Component : MonoBehaviour 
 	{
-		private float nextScan = 0.0f;
-		void Update()
-		{
-			if (Time.time <= nextScan) { return; }
-			nextScan += 0.3f;
-
-			Scene uiScene = SceneManager.GetSceneByName("UserInterface");
-			if (uiScene.loadingState == Scene.LoadingState.Loaded)
-			{
-				GameObject[] uiObjs = uiScene.GetRootGameObjects();
-				foreach (GameObject obj in uiObjs)
-				{
-					UI.HUD.EntityHighlighter[] ehis = obj.GetComponentsInChildren<UI.HUD.EntityHighlighter>(false);
-					foreach (UI.HUD.EntityHighlighter ehi in ehis)
-					{
-						ehi._text = "DEMON";
-						ehi.label.color = new Color(1f, 0f, 0f, 1f);
-						ehi.image.color = new Color(1f, 0f, 0f, 1f);
-					}
-				}
-			}
+		void Awake() {
+			Harmony.CreateAndPatchAll(typeof(DemonMode_Component));
 		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(UI.HUD.EntityHighlighter), nameof(UI.HUD.EntityHighlighter.Activate))]
+		static void OverrideActivate(UI.HUD.EntityHighlighter __instance) {
+			__instance._text = "DEMON";
+			__instance.label.color = new Color(1f, 0f, 0f, 1f);
+			__instance.image.color = new Color(1f, 0f, 0f, 1f);
+        }
 	}
 
-	[BepInPlugin("demon_mode", "Demon Mode", "0.0.1")]
+	[BepInPlugin("demon_mode", "Demon Mode", "0.0.3")]
 	public class DemonMode : BasePlugin
 	{
 		internal static new ManualLogSource Log;
